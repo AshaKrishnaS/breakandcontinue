db796ce6a4cc01ae8e4196c2538bcd8bbc511f0d
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-13 21:16:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace breakstatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 10; i++)
+
+            {
+
+                if (i == 5)
+
+                    continue;
+
+
+
+                if (i == 8)
+
+                    break;
+
+                Console.WriteLine("value is" + i);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
